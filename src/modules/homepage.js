d9833578a59c023bff6e53e1904bc2a0169dfc8b
hiwina <hiwina@gmail.com>
2023-05-27 01:48:47 +0300

    Add functionality for like and count

--- src/modules/homepage.js
+++ src/modules/homepage.js
@@ -1,31 +1,37 @@
-// Fetch data from TVmaze API
+import { getnumberofLikes, Interactlikebutton } from './likeItems.js';
+import itemsCounter from './itemCounter.js';
+
 const movieApi = async () => {
   const fetchResult = await fetch('https://api.tvmaze.com/shows');
   const ShowResult = await fetchResult.json();
-  return ShowResult.slice(0, 12);
+  const movies = ShowResult.slice(0, 16);
+  return movies;
 };
+movieApi();
+const movieCount = document.querySelector('.item-count');
 
 const movieList = async () => {
   const allMovies = await movieApi();
   allMovies.forEach((card) => {
     const CardContainer = document.querySelector('.card-container');
     const cardUL = document.createElement('ul');
-    cardUL.classList.add('movie-container');
     const cardLI = document.createElement('li');
     cardLI.className = 'movie-cards';
-    cardLI.innerHTML = `<div>
-                          <img src=${card.image.original} alt=${card.name}>
+    cardLI.innerHTML = `<div class="cards" id=${card.id}>
+                          <img src=${card.image.original} alt=${card.name} class="movie-img">
                         </div>
-                        <a href="${card.officialSite}" class="movie-title">${card.name}</a>
+                        <a href="${card.officialSite}" class="movie-title" data-id=${card.id}>${card.name}</a>
                         <div class="movie-info">
-                          <i class="fa fa-heart" aria-hidden="true"></i>
-                          <p>20 likes</p>
+                          <i class="fa fa-heart like-count like-btn" id ="lik"aria-hidden="true"></i> <p> likes</p>
                         </div>
-                        <button class="comment-btn" id=${card.id} >Comment</button>`;
+                        <button class="comment-btn" id="${card.id}" data-target="#popup-wrapper">Comments</button>`;
 
     cardUL.appendChild(cardLI);
     CardContainer.appendChild(cardUL);
   });
+  getnumberofLikes();
+  Interactlikebutton();
+  itemsCounter(allMovies, movieCount);
 };
 
 export { movieApi, movieList };
